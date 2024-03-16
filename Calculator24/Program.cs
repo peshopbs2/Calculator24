b36c8c4b7d234367ca73a9846694350cf5fd32ba
peshopbs2 <peshopbs2@gmail.com>
2024-03-16 14:10:01 +0200

    add user interface

--- Calculator24/Program.cs
+++ Calculator24/Program.cs
@@ -22,3 +22,34 @@ double Subtract(double a, double b)
 {
     return a - b;
 }
+
+Console.Write("a = ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("b = ");
+int b = int.Parse(Console.ReadLine());
+
+Console.Write("Operation (+, -, *, /): ");
+char op = Console.ReadKey().KeyChar;
+
+double result = 0;
+switch (op)
+{
+    case '+':
+        result = Add(a, b);
+        break;
+    case '-':
+        result = Subtract(a, b);
+        break;
+    case '*':
+        result = Multiply(a, b);
+        break;
+    case '/':
+        result = Divide(a, b);
+        break;
+    default:
+        Console.WriteLine("Invalid operation");
+        break;
+}
+Console.WriteLine();
+Console.WriteLine(result);
